6c737a9d373beee9c3258b42397e265318661fd1
lip10x <lipgms29@gmail.com>
2023-08-03 22:06:39 -0300

    exercicio 3

---
+++ Exercicios/Lista 1/ex3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <conio.h>
+
+/*3.	Escreva um programa que leia o código de origem de um produto e imprima na tela a região de sua procedência conforme a tabela abaixo:
+-código 1 : Sul 			-código 5 ou 6 : Nordeste
+-código 2 : Norte 			-código 7, 8 ou 9 : Sudeste
+-código 3 : Leste 			-código 10 : Centro-Oeste
+-código 4 : Oeste 			-código 11 : Noroeste
+*/
+
+
+	void main(){
+		
+		int i=1;
+		
+		printf("<<<Procura de produtos>>>");
+		
+		while (i!=0){
+			
+		printf("\ncodigo 1 : Sul");
+		printf("\ncodigo 2 : Norte");
+		printf("\ncodigo 3 : Leste");
+		printf("\ncodigo 4 : Oeste");
+		printf("\ncodigo 5 ou 6 : Nordeste");
+		printf("\ncodigo 7, 8 ou 9 : Sudeste");
+		printf("\ncodigo 10 : Centro-Oeste");
+		printf("\ncodigo 11 : Noroeste");
+		
+		}
+		
+		
+		
+	
+		
+		
+		
+		
+	}

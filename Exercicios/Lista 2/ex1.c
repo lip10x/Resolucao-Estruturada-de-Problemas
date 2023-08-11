33aae9eb5381b3343a961728dc616ea722990451
lip10x <lipgms29@gmail.com>
2023-08-10 22:04:56 -0300

    exercicio 1

---
+++ Exercicios/Lista 2/ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+
+//Escreva um procedimento que exiba a mensagem "olá mundo!". Este procedimento deverá ser chamado através do programa principal.
+
+
+void ola(){
+	printf("ola mundo!");
+}
+
+void main(){
+	ola();
+}

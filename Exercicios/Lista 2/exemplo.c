6066167546789e5caf3a2d0e6e5cd0086d20f833
lip10x <lipgms29@gmail.com>
2023-08-10 22:05:35 -0300

    exemplo

---
+++ Exercicios/Lista 2/exemplo.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <conio.h>
+
+void soma_(){
+	
+	int n1, n2, soma;
+	
+	n1 = 10;
+	n2 = 20;
+	soma = n1 + n2;
+	
+	printf("\nSoma: %d", soma);
+	
+	}
+	
+void sub_(){
+	
+	int n1, n2, sub;
+	
+	n1 = 10;
+	n2 = 20;
+	sub = n1 - n2;
+	
+	printf("\nSubtracao: %d", sub);
+	
+	}
+
+void main(){
+	
+	soma_();
+	sub_();
+	
+}

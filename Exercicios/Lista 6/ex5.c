7437334e160515f4faff153744021e99e48cd249
lip10x <lipgms29@gmail.com>
2023-08-17 22:02:52 -0300

    exercicio 5

---
+++ Exercicios/Lista 6/ex5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <conio.h>
+
+void menu(){
+	int opcao;
+	printf("Informe a categoria: ");
+	printf("\n1 - 5 a 7 anos");
+	printf("\n2 - 8 a 10 anos");
+	printf("\n3 - 11 a 13 anos");
+	printf("\n4 - 14 - 17 anos");
+	printf("\n5 - Mais de 18 anos");
+	printf("\n6 - Sair");
+	printf("\nInforme a categoria: ");
+	scanf("%d", &opcao);
+	
+	switch(opcao){
+		case 1:
+			
+			break;
+		case 2:
+			
+			break;
+		case 3:
+			
+			break;
+		case 4:
+			
+			break;
+		case 5:
+			
+			break;
+		default:
+			
+			break;
+	}
+}
+
+int
+
+
+void main(){
+	menu();
+}

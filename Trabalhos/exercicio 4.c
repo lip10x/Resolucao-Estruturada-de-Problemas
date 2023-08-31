6e030897e758b7ef5b10021fb704054647fb93cf
Felipe Miguel Mata <131825635+lip10x@users.noreply.github.com>
2023-08-31 03:03:59 -0300

    exercicio 4

--- Trabalhos/exercicio 4.c
+++ Trabalhos/exercicio 4.c
@@ -6,27 +6,48 @@
 
 
 
-float salario_bruto(){
+float salario_bruto(float *sb){
 	
-	float sb;
-	
-	printf("Digite o salario bruto: ");
-	scanf("%f", sb);
+	printf("\nDigite o salario bruto: ");
+	scanf("%f", &sb);
 	
 	
 }
 
+float horas_extras(float *hrs){
+	
+	printf("\nDigite o total de horas extras: ");
+	scanf("%f", &hrs);
+	
+}
 
+float descontos(float *dsc){
+	
+	printf("\nDigite o total de descontos: ");
+	scanf("%f", &dsc);
+	
+}
 
-
-
-
-
-
-
+float ganho_horas(float hrs) {
+    float ganho_total = hrs * 20;
+    return ganho_total;
+}
 
 
 void main(){
 	
-	salario_bruto()
+	float sb, hrs, dsc, ganho;
+	
+	
+	printf("Salario bruto: %0.2f", salario_bruto(&sb));
+	printf("Horas extras trabalhadas: %0.2f", horas_extras(&hrs));
+	printf("Descontos recebidos: %0.2f", descontos(&dsc));
+	printf("\nGanho por horas extras: %0.2f", ganho_horas(hrs));
+	
+	
 }
+
+
+
+
+

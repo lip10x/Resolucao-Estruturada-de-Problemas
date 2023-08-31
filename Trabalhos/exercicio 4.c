863706abaf4160f8f15d96029c0523d4d03b1a4b
Felipe Miguel Mata <131825635+lip10x@users.noreply.github.com>
2023-08-31 01:32:28 -0300

    começo exercicio 4

---
+++ Trabalhos/exercicio 4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+
+
+
+
+
+
+float salario_bruto(){
+	
+	float sb;
+	
+	printf("Digite o salario bruto: ");
+	scanf("%f", sb);
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+void main(){
+	
+	salario_bruto()
+}

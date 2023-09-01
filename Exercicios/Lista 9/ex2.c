7510b3961ab20fdba4de20f2f82694b4f581b23e
lip10x <lipgms29@gmail.com>
2023-08-31 21:50:26 -0300

    exercicio 2

---
+++ Exercicios/Lista 9/ex2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <conio.h>
+
+#define TF 5
+
+void carregar_vetor(int vetor[TF]){
+	int i = 0;
+	printf("\n\n<<<Carregar Vetores>>>\n\n");
+	system("pause");
+	for(i=0 ; i<TF ; i++){
+		printf("\nInsira um numero para o vetor[%d]: ", i);
+		scanf("%d", &vetor[i]);
+	}
+}
+
+void exibir_vetor(int vetor[TF]){
+	int i;
+	printf("\n\n<<<Exibir Vetor>>>\n\n");
+	system("pause");
+	for(i=0 ; i<TF ; i++){
+		printf("\nVetor[%d] = %d", i, vetor[i]);
+	}
+}
+
+void main(){
+	int vetor[TF];
+	carregar_vetor(vetor);
+	exibir_vetor(vetor);
+}

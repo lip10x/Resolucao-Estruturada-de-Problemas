bb50b20d5df41586a71ccd6b9d4d9144d242b18c
Felipe Miguel Mata <131825635+lip10x@users.noreply.github.com>
2023-09-21 01:42:51 -0300

    exercício 9

---
+++ Trabalhos/trabalho_vetor/ex9.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+
+#define TF 5
+
+
+void main(){
+	int vetor[TF]
+	
+	printf("Insira 30 valores: ");
+	for(i=0;i<TF;i++){
+		scanf("%d", &vetor[i]);
+	}
+	
+}

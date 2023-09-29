5694296f94276fa750689840ed01efacb5434f37
Felipe Miguel Mata <131825635+lip10x@users.noreply.github.com>
2023-09-29 01:38:55 -0300

    exercicio 4 finalizado

--- Trabalhos/trabalho_matriz/ex4.c
+++ Trabalhos/trabalho_matriz/ex4.c
@@ -8,33 +8,35 @@
 
 
 
-void carregar_matriz(int mat[TL][TC]){
-	int l,c, cont_maior_que_dez;
+void carregar_matriz(int mat[TL][TC], int mat2[TL][TC], int mat3[TL][TC]){
+	int l,c;
 	
 	for(l=0;l<TL;l++){
 		for(c=0;c<TC;c++){
 			printf("Informe um numero para a matriz[%d][%d]: ", l,c);
 			scanf("%d", &mat[l][c]);
-			if(mat[l][c]>10){
-				cont_maior_que_dez++;
-			}
 		}
 	}
-	printf("Qtd de numeros maiores que 10: %d", cont_maior_que_dez);
-}
-
-void exibir_matriz(int mat[TL][TC]){
-	int l,c;
 	for(l=0;l<TL;l++){
 		for(c=0;c<TC;c++){
-			printf("\nMatriz[%d][%d]= %d ", l, c, mat[l][c]);
+			printf("Informe um numero para a matriz2[%d][%d]: ", l,c);
+			scanf("%d", &mat2[l][c]);
+			if(mat2[l][c]>mat[l][c]){
+				mat3[l][c] = mat2[l][c];
+			}
+			else mat3[l][c] = mat[l][c];
+			}
+			
+		}
+	for(l=0;l<TL;l++){
+		for(c=0;c<TC;c++){
+			printf("\nMatriz[%d][%d]= %d ", l, c, mat3[l][c]);
 		}
 	}
-}
+	}
 
 
 void main(){
 	int mat[TL][TC], mat2[TL][TC], mat3[TL][TC];
-	carregar_matriz(mat);
-	exibir_matriz(mat);
+	carregar_matriz(mat,mat2,mat3);
 }

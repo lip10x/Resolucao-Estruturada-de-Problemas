6d4c064d8a6d29a5b8de97fa545e1f5b168ea0f2
Felipe Miguel Mata <131825635+lip10x@users.noreply.github.com>
2023-09-29 01:09:13 -0300

    exercicio 4

---
+++ Trabalhos/trabalho_matriz/ex4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <conio.h>
+
+
+#define TL 4
+#define TC 4
+
+
+
+
+void carregar_matriz(int mat[TL][TC]){
+	int l,c, cont_maior_que_dez;
+	
+	for(l=0;l<TL;l++){
+		for(c=0;c<TC;c++){
+			printf("Informe um numero para a matriz[%d][%d]: ", l,c);
+			scanf("%d", &mat[l][c]);
+			if(mat[l][c]>10){
+				cont_maior_que_dez++;
+			}
+		}
+	}
+	printf("Qtd de numeros maiores que 10: %d", cont_maior_que_dez);
+}
+
+void exibir_matriz(int mat[TL][TC]){
+	int l,c;
+	for(l=0;l<TL;l++){
+		for(c=0;c<TC;c++){
+			printf("\nMatriz[%d][%d]= %d ", l, c, mat[l][c]);
+		}
+	}
+}
+
+
+void main(){
+	int mat[TL][TC], mat2[TL][TC], mat3[TL][TC];
+	carregar_matriz(mat);
+	exibir_matriz(mat);
+}

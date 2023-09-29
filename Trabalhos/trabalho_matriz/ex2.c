2edd105f36a6683890a492b466f86a267c461afd
Felipe Miguel Mata <131825635+lip10x@users.noreply.github.com>
2023-09-29 00:55:22 -0300

    exercício 2

---
+++ Trabalhos/trabalho_matriz/ex2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdio.h>
+
+
+#define TL 5
+#define TC 5
+
+
+
+
+void carregar_matriz(int mat[TL][TC]){
+	int l,c;
+	
+	for(l=0;l<TL;l++){
+		for(c=0;c<TC;c++){
+			if(l==c){
+				mat[l][c] = 1;
+			}
+			else mat[l][c] = 0;
+			}
+		}
+	}
+
+void exibir_matriz(int mat[TL][TC]){
+	int l,c;
+	printf("Como a matriz ficou: \n");
+	for(l=0;l<TL;l++){
+		for(c=0;c<TC;c++){
+			printf("%d ", mat[l][c]);
+		}
+		printf("\n");
+	}
+}
+
+
+void main(){
+	int mat[TL][TC];
+	carregar_matriz(mat);
+	exibir_matriz(mat);
+}

17e8efd137b8162345caebfa5ed772355cf7853d
Felipe Miguel Mata <131825635+lip10x@users.noreply.github.com>
2023-09-29 01:39:02 -0300

    exercicio 5

---
+++ Trabalhos/trabalho_matriz/ex5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <conio.h>
+
+
+#define TL 3
+#define TC 3
+
+
+
+
+void carregar_matriz(int mat[TL][TC]){
+	int l,c, media;
+	
+	for(l=0;l<TL;l++){
+		for(c=0;c<TC;c++){
+			printf("Digite um numero para a matriz [%d] [%d]: ", l, c);
+			scanf("%d", &mat[l][c]);
+			if(l==c){
+				media += mat[l][c] ;
+			}
+		}
+	}
+	media = media / TL;
+	printf("Media dos numeros da diagonal principal: %d", media);
+}
+
+
+
+
+void main(){
+	int mat[TL][TC];
+	carregar_matriz(mat);
+}

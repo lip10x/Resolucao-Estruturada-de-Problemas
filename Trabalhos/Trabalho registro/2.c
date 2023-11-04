0124e85678709a62bc87d1973dfa145ea0d8f94d
Felipe Miguel Mata <131825635+lip10x@users.noreply.github.com>
2023-11-04 13:42:27 -0300

    exercicio 2 completo

--- Trabalhos/Trabalho registro/2.c
+++ Trabalhos/Trabalho registro/2.c
@@ -33,8 +33,8 @@ void carregar_reg(struct alunos reg_cad[TF]){
 }
 
 void media(struct alunos reg_cad[TF]){
-	int i; 
-	float med_maior = 0, med_menor = reg_cad[0].media;
+	int i, cont_maior_que_sete = 0, cont_menor_que_sete = 0, cont_faltas = 0; 
+	float med_maior = 0, med_menor = reg_cad[0].media, media_geral, cont_geral;
 	char nome_maior[50], nome_menor[50];
 	for(i=0;i<TF;i++){
 		if(reg_cad[i].media > med_maior){
@@ -45,21 +45,25 @@ void media(struct alunos reg_cad[TF]){
 			med_menor = reg_cad[i].media;
 			strcpy(nome_menor,reg_cad[i].nome);
 		}
+		if(reg_cad[i].media >= 7){
+			cont_maior_que_sete++;
+		}
+		else if(reg_cad[i].media < 7){
+			cont_menor_que_sete++;
+		}
+		if(reg_cad[i].faltas > 20){
+			cont_faltas++;
+		}
+		cont_geral += reg_cad[i].media;
 	}
+	media_geral = cont_geral/TF;
 	printf("\n%s ficou com media: %0.2f e eh a maior nota!\n", nome_maior, med_maior);
 	printf("\n%s ficou com media: %0.2f e eh a menor nota\n", nome_menor, med_menor);
+	printf("\nMedia geral da turma: %0.2f: \n", media_geral);
+	printf("\nQtd de alunos que ficaram com mais de 7 de media: %d", cont_maior_que_sete);
+	printf("\nQtd de alunos que ficaram com menos de 7 de media: %d\n", cont_menor_que_sete);
+	printf("\nQtd de alunos que ficaram com mais de 20 faltas: %d\n\n", cont_faltas);
 }
-
-/*void salario_maior(struct alunos reg_cad[TF]){
-	int i;
-	float maior_sal=0;
-	for(i=0;i<TF;i++){
-		if(reg_cad[i].salario > maior_sal){
-			maior_sal = reg_cad[i].salario;
-		}
-	}
-	printf("\nMaior Salario: %0.2f\n", maior_sal);
-}*/
 
 void exibir_reg(struct alunos reg_cad[TF]){
 	int i, cont_fem=0;
@@ -77,6 +81,5 @@ void main(){
 	struct alunos reg_cad[TF];
 	carregar_reg(reg_cad);
 	media(reg_cad);
-	//salario_maior(reg_cad);
 	exibir_reg(reg_cad);
 }
